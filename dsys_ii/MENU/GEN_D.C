99c554948a4e682e03c05acccf681e9ab66bd1ea
Dmitry Kokorev <freeduke33@gmail.com>
2017-02-18 13:09:41 +0400

    archive sources commit

---
+++ dsys_ii/MENU/GEN_D.C
@@ -0,0 +1,9 @@
+
+#include "dsys.h"
+#include "tcxlwin.h"
+
+VOID CTYP GEN_D(NOARG)
+{
+ Wperror("режим не поддерживается");
+ return;
+}
---
+++ dsys_ii/MENU/ZVENO_D.C
@@ -0,0 +1,9 @@
+
+#include "dsys.h"
+#include "tcxlwin.h"
+
+VOID CTYP ZVENO_D(NOARG)
+{
+ Wperror("режим не поддерживается");
+ return;
+}
